700fac9e73816132d05c3ed9bb61d6f3e22f55ff
Bulent Tayfun <81321769+tayfunbulent@users.noreply.github.com>
2022-09-12 00:00:06 +0300

    Add files via upload

---
+++ src/components/lifeCycle.js
@@ -0,0 +1,24 @@
+/*import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+class LifeCycle extends Component {
+    componentDidMount(){
+        console.log('Executed for last DOM.');
+        console.log(ReactDOM.findDOMNode(this));
+        const data = JSON.parse(localStorage.getItem("data")) || [];
+    }
+    render() {
+        const data = JSON.parse(localStorage.getItem("data")) || [];
+        return (
+            <div className="App">
+                <h1>React DOM</h1>
+                <script>console.log('sesasasasasas');</script>
+            </div>
+        );
+    }
+}
+
+export default LifeCycle;
+*/
